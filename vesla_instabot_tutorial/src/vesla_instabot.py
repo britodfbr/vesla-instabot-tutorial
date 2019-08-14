825115e38f0407aebdb7e6fecc09a3be2e7f15b2
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2019-08-14 01:26:26 -0300

    vesla_instabot: add VeslaInstaBot constructor

---
+++ vesla_instabot_tutorial/src/vesla_instabot.py
@@ -0,0 +1,8 @@
+import requests
+from .endpoints import endpoints
+
+class VeslaInstaBot:
+    def __init__(self):
+        self.session = requests.Session()
+        r = self.session.get(endpoints['base'])
+        self.session.headers.update({'X-CSRFToken': r.cookies['csrftoken']})
