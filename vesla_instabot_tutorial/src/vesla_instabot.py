8310b7f6a50ea03e1e4515c26cf1afe31bdfcdb6
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2019-08-14 01:40:27 -0300

    vesla_instabot: add login method

--- vesla_instabot_tutorial/src/vesla_instabot.py
+++ vesla_instabot_tutorial/src/vesla_instabot.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from .endpoints import endpoints
 
 class VeslaInstaBot:
@@ -7,3 +8,12 @@ class VeslaInstaBot:
         r = self.session.get(endpoints['base'])
         self.session.headers.update({'X-CSRFToken': r.cookies['csrftoken']})
         self.user_id = None
+
+
+    def login(self, credentials):
+        r = self.session.post(endpoints['login'], data=credentials)
+        self.session.headers.update({'X-CSRFToken': r.cookies['csrftoken']})
+        r = json.loads(r.content.decode('utf-8'))
+        if r['authenticated']:
+            self.user_id = r['userId']
+        return r
