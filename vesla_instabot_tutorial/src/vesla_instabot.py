4e13256077e8b235375d15ceca0ab6a81970f934
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2019-08-14 01:39:37 -0300

    vesla_instabot: add user_id attribute

--- vesla_instabot_tutorial/src/vesla_instabot.py
+++ vesla_instabot_tutorial/src/vesla_instabot.py
@@ -6,3 +6,4 @@ class VeslaInstaBot:
         self.session = requests.Session()
         r = self.session.get(endpoints['base'])
         self.session.headers.update({'X-CSRFToken': r.cookies['csrftoken']})
+        self.user_id = None
